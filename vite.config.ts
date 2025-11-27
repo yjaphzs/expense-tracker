74bc6aec57edfa3dea37afa4bfe14a7976482990
Jan Bautista <62127854+yjaphzs@users.noreply.github.com>
2025-11-28 04:20:22 +0800

    Initial setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite"
+import { VitePWA } from 'vite-plugin-pwa'
+import pkg from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                cleanupOutdatedCaches: true,
+            },
+        }),
+    ],
+    define: {
+        __APP_VERSION__: JSON.stringify(pkg.version),
+    },
+    build: {
+        outDir: 'build',
+        minify: 'terser',
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                },
+                entryFileNames: `assets/[name]-[hash].js`, 
+                chunkFileNames: `assets/[name]-[hash].js`,
+                assetFileNames: `assets/[name]-[hash].[ext]`,
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    base: "/expense-tracker/",
 })
